ca6b407b8b08aecb25f6fd33f4a995b63e324b62
Phan Hieu <hieutrunglephan@gmail.com>
2018-10-14 13:22:16 +0700

    Squashed commit of the following:
    commit 964708ed7982222d42c3fef85f045e7f5c753ca8
    Author: Phan Hieu <hieutrunglephan@gmail.com>
    Date:   Sun Oct 14 13:19:39 2018 +0700
    
        add banner
    
    commit 0a27c1a549e9dbbfec74d5600570d6f268ded8ac
    Author: Phan Hiếu <hieutrunglephan@gmail.com>
    Date:   Sun Sep 16 01:35:37 2018 +0700
    
        [Feature] Update github template (#9)
    
    commit 913051d16ac708ae8839c83f2a4be8fb8a3810eb
    Author: Phan Hiếu <hieutrunglephan@gmail.com>
    Date:   Sun Sep 16 01:31:40 2018 +0700
    
        [Feature] GitHub template (#8)
    
        ### Status
    
        READY
    
        ### What's this PR do?
    
        - [x] Add bug report template
        - [x] Add feature request template
        - [x] Add conduct
        - [x] Add license
        - [x] Add PR template

--- cmd/boss/main.go
+++ cmd/boss/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"log"
 
+	"github.com/dimiro1/banner"
+	colorable "github.com/mattn/go-colorable"
 	"github.com/phanletrunghieu/bot-net/boss/service/cli"
 	"github.com/phanletrunghieu/bot-net/boss/service/tcp"
 )
 
 func main() {
+	welcome := "\r\n    __  ___            ____           ______           _ \r\n   / / / (_)__  __  __/ __ \\___  ____/_  __/________ _(_)\r\n  / /_/ / / _ \\/ / / / / / / _ \\/ __ \\/ / / ___/ __ `/ / \r\n / __  / /  __/ /_/ / /_/ /  __/ /_/ / / / /  / /_/ / /  \r\n/_/ /_/_/\\___/\\__,_/_____/\\___/ .___/_/ /_/   \\__,_/_/   \r\n                             /_/                         \r\n"
+	isEnabled := true
+	isColorEnabled := true
+	banner.Init(colorable.NewColorableStdout(), isEnabled, isColorEnabled, bytes.NewBufferString(welcome))
+
 	bossService := tcp.NewTCPService("127.0.0.1", 8081)
 	go func() {
 		err := <-bossService.Error
